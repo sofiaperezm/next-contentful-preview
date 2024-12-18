bc7de0a51e1e2cfbf7ce65192369ace3dbe87cb4
sofiaperezm <sofi.prz.m@gmail.com>
2024-12-18 17:53:30 -0500

    initial commit

--- app/page.tsx
+++ app/page.tsx
@@ -13,7 +13,7 @@ function Intro() {
   return (
     <section className="flex-col md:flex-row flex items-center md:justify-between mt-16 mb-16 md:mb-12">
       <h1 className="text-6xl md:text-8xl font-bold tracking-tighter leading-tight md:pr-8">
-        Blog.
+        CatLovers
       </h1>
       <h2 className="text-center md:text-left text-lg mt-5 md:pl-8">
         A statically generated blog example using{" "}
